5bfa3a218179f1aa9e2d9061f245123269d87b93
sboyls007 <45946829+sboyls007@users.noreply.github.com>
2018-12-17 11:03:53 -0600

    Add files via upload

---
+++ Homework 1 FavoriteSong.py
@@ -0,0 +1,18 @@
+SongTitle = "Home By The Sea"
+Artist = "Genesis"
+YearReleased = 1993"
+RecordLabel = 'Atlantic Records"
+Genere = "Pop"
+DurationSeconds = 510
+AlbumTitle = "The Way We Walk: The Longs"
+RecordingType = "Live"
+
+print(SongTitle)
+print(Artist)
+print(YearReleased)
+print(RecordLabel)
+print(Genere)
+print(DurationSeconds)
+print(AlbumTitle)
+print(RecordingType)
+
